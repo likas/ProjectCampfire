4454f04ad32993d41b47982ddbe9143ec907fe40
liliya.cheremnykh@gmail.com <liliya.cheremnykh@gmail.com>
2021-11-21 19:18:30 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProjectCampfire
+
+Developed with Unreal Engine 4
---
+++ Source/ProjectCampfire/ProjectCampfireGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectCampfireGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTCAMPFIRE_API AProjectCampfireGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
